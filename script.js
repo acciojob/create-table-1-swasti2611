78ee63bf8b43482b362f274adde497510108f498
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-04-10 21:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function insert_Row() {
     //Write your code here
+let index=2;
   let table = document.getElementById("sampleTable")
 	let tr = document.createElement("tr")
 	let td1 = document.createElement("td")
 	let td2 = document.createElement("td")
-  td1.innerText = "New Cell1"
+     td1.innerText = "New Cell1"
 	td2.innerText = "New Cell2"
 	tr.appendChild(td1)
 	tr.appendChild(td2)
